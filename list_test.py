3b0f200a41316bee18906caffc67840897d407ef
yangliubo <315587535@qq.com>
2022-03-30 00:55:34 +0800

    补充list相关操作

--- list_test.py
+++ list_test.py
@@ -5,7 +5,7 @@
 @Author  :   希瓦 
 '''
 
-# 创建list
+# 一、创建list
 # 1、直接定义
 list_1 =  [1,'m',(1,2)]
 # 2、列表推导式
@@ -13,3 +13,29 @@ list_2 = [x**3 for x in range(3)]
 print(list_2) 
 list_3 = list(map(lambda x:x*3, range(3)))
 print(list_3)
+
+# 二、列表与string互转
+# 1、str to list
+str_1 = 'abcde'
+list_1 = list(str_1)
+print(list_1)
+
+str_2 = '8548775'
+list_2 = list(str_2)
+print(list_2)
+
+# 2、list to str
+# 方法1：''.join(list) 注意 该方法中list的元素必须是str类型，非str类型的如Int类型必须先转为str类型
+
+list_1 = ['a','b','c','d','e']
+str_1 = ''.join(list_1)
+print(str_1)
+
+
+list_2 = [1,2,3,4,5]
+str_2 = ''.join([str(x) for x in list_2])
+print(str_2)
+
+# 三、list相关操作
+
+# 四、list的一些方法
